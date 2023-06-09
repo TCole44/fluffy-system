81c565f1f9cd08aabf80f35b43dd92964a17abe9
TCole44 <marcustcoleman4@gmail.com>
2023-06-08 22:17:45 -0400

    connect to jawsdb

--- seeds/songData.js
+++ seeds/songData.js
@@ -18,31 +18,37 @@ const songData = [
           "song": "Me Porto Bonito",
           "album": "Un Verano Sin Ti",
           "genre": "Pop"
-        }
+        },
         {
           "artist": "Outkast",
           "song": "Flip Flop Rock (Feat. Killer Mike & JAY Z)",
           "album": "Speakerboxxx/ The Love Below",
           "genre": "Hiphop/Rap"
-        }
+        },
         {
           "artist": "Mac Miller",
           "song": "2009 ",
           "album": "Swimming",
           "genre": "Hiphop/Rap"
-        }
+        },
         {
           "artist": "Jorja Smith",
           "song": "Let Me Down(Feat. Stormzy)",
           "album": "Let Me Down - Single",
           "genre": "Pop"
-        }
+        },
         {
           "artist": "21 Savage",
           "song": "Glock in My Lap",
           "album": "Savage Mode 2",
           "genre": "Hiphop/Rap"
         }
+        {
+          "artist": "Lil Wayne",
+          "song": "Receipt",
+          "album": "Carter II",
+          "genre": "Hiphop/Rap"
+        },
 ]
 
 module.exports = seedSongs;
