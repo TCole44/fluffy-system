156e4db569d7ca427445655851de74405ffb493d
Jojhan Silva <silvajojhan@gmail.com>
2023-06-12 19:54:49 -0500

    added homepage route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
+const { Playlists, Songs } = require('../models')
 
 
+// GET all galleries for homepage
+router.get('/', (req, res) => {
+  res.render('homepage');
+});
 
 
 module.exports = router;
