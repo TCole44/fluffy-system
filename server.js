a5da03ad45fce4feb502d67de1d7d26b37384284
TCole44 <marcustcoleman4@gmail.com>
2023-06-14 21:02:58 -0400

    added code to server.js

--- server.js
+++ server.js
@@ -31,7 +31,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+app.get("/songs/:song", function (req, res) {
+  console.log(req.params.model);
+  for (let i = 0; i < allSongs.length; i++) {
+    if (allSongs[i].song.toLowerCase() === req.params.model.toLowerCase()) {
+      req.session.save(() => {
+        req.session.lastModel = req.params.model;
+        req.session.count = req.session.count + 1;
+      });
 
+      return res.render("song", allSongs[i]);
+    }
+  }
+});
+
+app.get("/songsbyid/:id", function (req, res) {
+  return res.render("song", allSongs[req.params.id - 1]);
+});
+
+app.get(["/", "/allSongs"], function (req, res) {
+  res.render("allsongs", {
+    songArray: allSongs,
+    song: "",
+    artist: "",
+    lastModel: req.session.lastModel,
+  });
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
