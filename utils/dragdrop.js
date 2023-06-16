68c882a541cf18ba812c51d494ff0227d52c5cfa
TCole44 <marcustcoleman4@gmail.com>
2023-06-15 22:30:16 -0400

    added code for playlist functionality

---
+++ utils/dragdrop.js
@@ -0,0 +1,20 @@
+const react = require('react')
+
+const playlist = [
+    {
+        id: '',
+        song: {song},
+        artist: {artist},
+        album: {album}
+    }
+]
+
+function dragDrop() {
+    return(
+        <div className='playist-item'>
+
+        </div>
+    )
+}
+
+export default dragDrop
--- utils/helper.js
+++ utils/helper.js
@@ -1,5 +1,16 @@
-module.exports = {
-    songLinkTitle: function (song, artist, url) {
-      return `${song} by ${artist}`;
-    },
-  };
+const {DndProvider } = require('react-dnd');
+const { HTML5Backend } = require('react-dnd-html5-backend');
+const {dragDrop} = require('./dragdrop')
+
+function playlist() {
+    render() 
+      return (
+        <DndProvider backend={HTML5Backend}>
+        <div className='playlist-item'>  
+             <h3><a href="/songs/{{this.dataValues.id}}">{{this.dataValues.song}}</a></h3>
+              <p>Artist: {{this.dataValues.artist}}</p>
+        </div>
+        </DndProvider>
+      )};
+
+export default playlist
