fb5b4b73c4edea880c5825597c4565831c3a267c
Jojhan Silva <silvajojhan@gmail.com>
2023-06-15 19:24:17 -0500

    playlists and login button links fixed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const songRoutes = require('./songRoutes');
+const playlistRoutes = require('./playlistRoutes')
 
 router.use('/songs', songRoutes);
+router.use('/playlists', playlistRoutes)
 
 module.exports = router;
