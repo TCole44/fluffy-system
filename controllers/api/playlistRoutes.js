c0988fbab92a852c18117ebb0dfe1894a3e4d739
Jojhan Silva <silvajojhan@gmail.com>
2023-06-15 19:31:37 -0500

    fixed playlist button

--- controllers/api/playlistRoutes.js
+++ controllers/api/playlistRoutes.js
@@ -2,6 +2,18 @@ const router = require('express').Router();
 const { start } = require('repl');
 const { Playlists } = require('../../models');
 
+router.get('/', async (req,res) => {
+    try{
+    res.render('allplaylists');
+    }  
+    
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+
+
+})
 
 
 module.exports = router;
