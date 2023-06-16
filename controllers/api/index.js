f63dedb5cc45ccc1244e2e46516a631845381853
Jojhan Silva <silvajojhan@gmail.com>
2023-06-15 20:32:38 -0500

    moved login routes from the server.js to its own routes file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const songRoutes = require('./songRoutes');
 const playlistRoutes = require('./playlistRoutes')
+const userRoutes = require('./userRoutes')
 
 router.use('/songs', songRoutes);
 router.use('/playlists', playlistRoutes)
+router.use('/login', userRoutes)
+router.use('/register', userRoutes)
 
 module.exports = router;
