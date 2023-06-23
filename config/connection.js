291e44fcddec8fc87c9a24305916a9d3943a1e8d
TCole44 <marcustcoleman4@gmail.com>
2023-06-22 22:51:39 -0400

    updated code

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,21 @@
-  const Sequelize = require("sequelize");
-  require("dotenv").config();
-  
-  let sequelize;
-  
-  // JAWSDB_URL is created when we setup the mySQL JAWs Add On.
-  // If its not there, you are probably in your local
-  if (!process.env.JAWSDB_URL) {
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+let sequelize;
+
+if (!process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
     sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: process.env.DB_HOST,
-        dialect: "mysql",
-        port: 3306,
-      }
-    );
-  } else {
-    //Jaws DB Production URL
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-  }
-  
-  module.exports = sequelize;
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
+module.exports = sequelize;
