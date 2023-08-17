e0625d9d6f6988bc634c8ce7c60d64b911653dbd
neysap <porterneysa@gmail.com>
2023-08-16 21:33:41 -0400

    updated api

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-// const { User } = require('../models');
-// const withAuth = require('../utils/auth');
-// const { Playlists, Songs } = require('../models')
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+const { Playlists, Songs } = require('../models')
 
 
 
